9994a57a7e40fee8161ea775690488d499cf1876
Ekrem <ekremteke.uk@gmail.com>
2022-10-15 08:52:10 +0100

    exercise task 1 was done

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -15,7 +15,21 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+let pQ = document.querySelectorAll("p");
+let p = document.getElementsByTagName("p");
+console.log(p);
+console.log(pQ);
 
+let div = document.getElementsByTagName("div");
+console.log(div);
+
+let jumbotron = document.getElementById("jumbotron-text");
+console.log(jumbotron);
+
+let pInPrimary = document.querySelectorAll(".primary-content");
+let x = pInPrimary.querySelectorAll("p");
+console.log(pInPrimary);
+console.log(x);
 
 /*
 Task 2
@@ -24,7 +38,6 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
-
 /*
 Task 3
 =======
@@ -32,15 +45,12 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
-
 /*
 Task 4
 ======
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-
-
 
 /*
 Task 5
