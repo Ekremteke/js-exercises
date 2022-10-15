2a61c32daf1f3d61efec0c85da95056e48db0d6d
Ekrem <ekremteke.uk@gmail.com>
2022-10-15 16:33:58 +0100

    task 1-2-3-4 were done

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -26,10 +26,12 @@ console.log(div);
 let jumbotron = document.getElementById("jumbotron-text");
 console.log(jumbotron);
 
-let pInPrimary = document.querySelectorAll(".primary-content");
-let x = pInPrimary.querySelectorAll("p");
+let pInPrimary = document.querySelector(".primary-content p");
 console.log(pInPrimary);
-console.log(x);
+
+// let x = pInPrimary.querySelectorAll("p");
+
+// console.log(x);
 
 /*
 Task 2
@@ -37,20 +39,49 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+let myElement = document.querySelector("#alertBtn");
+
+console.log(myElement);
+
+myElement.addEventListener("click", alertThanks);
+
+function alertThanks() {
+  alert("Thanks for visiting Bikes for Refugees!");
+}
+
+// myElement.style.backgroundColor = "red";
 
 /*
 Task 3
-=======
-
+======
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
+let changeColour = document.querySelector("#bgrChangeBtn");
+console.log(changeColour);
+let body1 = document.querySelector("body");
+console.log(body1);
+changeColour.addEventListener("click", changeBacground);
+function changeBacground() {
+  body1.style.backgroundColor = "red";
+}
 /*
 Task 4
 ======
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
+let addSomeTextBtn = document.querySelector("#addTextBtn");
+
+let underBtn = document.querySelector(".buttons");
+
+addSomeTextBtn.addEventListener("click", addTextFunction);
+
+let para = document.createElement("p");
+
+function addTextFunction() {
+  underBtn.appendChild(para);
+  para.innerText = "Read more below.";
+}
 
 /*
 Task 5
